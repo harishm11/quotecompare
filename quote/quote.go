2d546ac6146de845655dab3ab94d759d873ac839
harishm11 <harishm11@gmail.com>
2022-08-23 22:20:54 -0700

    test commit

--- quote/quote.go
+++ quote/quote.go
@@ -111,6 +111,7 @@ func NewQuote(c *fiber.Ctx) {
 	//DTO for rating variables - parent process - P2 - create generic and company specific rating variables
 	//Concurrent 100 company quotes - child processes - PD
 	//each child process for compaany gets calc rule for each coverage from redis cache , get the factors , calc the premium, send premium response - P3
+	//other modules
 
 	db.Create(&quote)
 	c.JSON(quote)
