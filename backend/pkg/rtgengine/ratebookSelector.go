a66a23326bad214bbbc48c513c017b8e4224fb0b
harishm11 <harishm11@gmail.com>
2022-09-19 17:25:26 -0700

    09-19-2022

--- backend/pkg/rtgengine/ratebookSelector.go
+++ backend/pkg/rtgengine/ratebookSelector.go
@@ -11,8 +11,8 @@ import (
 func RatebookSelector(q *models.Quote) {
 	db := database.DBConn
 	var result tables.Ratebook
-	db.Table("ratebooks").Where("ratebook_activation_date < ? AND ratebook_new_business_eff_date <= ? ",
-		q.RateAppliedDate, q.QuoteEffDate).Last(&result)
+	db.Table("ratebooks").Where("ratebook_activation_date <= ? AND ratebook_new_business_eff_date <= ? ",
+		q.RateAppliedDate, q.QuoteEffDate).Order("ratebook_activation_date desc").Last(&result)
 	fmt.Println(result.RatebookCode)
 	fmt.Println(q.RateAppliedDate)
 	fmt.Println(q.QuoteEffDate)
--- backend/pkg/tables/tables.go
+++ backend/pkg/tables/tables.go
@@ -3,8 +3,16 @@ package tables
 import "time"
 
 type Ratebook struct {
+	ProductCode                string `gorm:"primaryKey"`
 	RatebookCode               string `gorm:"primaryKey"`
 	RatebookActivationDate     time.Time
 	RatebookNewBusinessEffDate time.Time
 	RatebookRenBusinessEffDate time.Time
+}
+
+type Routine struct {
+	RoutineID    string `gorm:"primaryKey"`
+	CoverageCode string
+	RoutineName  string
+	RatebookCode string
 }
